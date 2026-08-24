4d53d6c81ed83f360ea7373061dfd7bbbdd7f9a6
Yash Khartode <yashkhartode1004@gmail.com>
2026-08-24 22:36:23 +0530

    Star.

--- 10_NestedLoops.cpp
+++ 10_NestedLoops.cpp
@@ -8,6 +8,24 @@ int main(){
         }                        // 3     3<=4   1,2,3,4,5    1,2,3,4,(5 no)<=4     3 3 3 3     
         cout<< endl;             // 4     4<=4   1,2,3,4,5    1,2,3,4,(5 no)<=4     4 4 4 4
     }                            // 5     5<=4 No....
+
+    cout<< "Star Pattern."<< endl;
+    int n = 4;
+    for(int i=1; i<=n; i++){      // n=4   j<=i      "*"
+        for(int j=1; j<=i; j++){  //  4       1      *
+            cout<< "* ";          //  4       2      **
+        }                         //  4       3      ***
+        cout<< endl;              //  4       4      ****
+    }
+
+    cout<< "Inverted Star Pattern."<< endl;
+    for(int i=1; i<=n; i++){           // n=4   j<= n-i+1       "*"
+        for(int j=1; j<=n-i+1; j++){   //  4        4-1+1 = 4   ****
+            cout<< "* ";               //  4        4-2+1 = 3   ***
+        }                              //  4        4-3+1 = 2   **
+        cout<< endl;                   //  4        4-4+1 = 1   *
+    }
+
     return 0;
 }
 
@@ -18,6 +36,6 @@ int main(){
   R2 2  2  2  2  (4 -> 2 prints) (4) -> Rows
   R3 3  3  3  3  (4 -> 3 prints)
   R4 4  4  4  4  (4 -> 4 prints)
-2. Inner Loop: It decides to print Number of columns/each row. What to print in each row Do it in inner loop.
+2. Inner Loop: It decides to print Number of columns/in each row. What to print in each row Do it in inner loop.
 3. Work in inner loop: Basically work in Each Row.
 */
