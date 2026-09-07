0dcfdcfa466e77564660acf5e8238d9857594afe
Yash Khartode <yashkhartode1004@gmail.com>
2026-09-07 21:27:52 +0530

    Function call.

---
+++ 12_Function_call.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b){
+    int s = a+b;
+    return s;
+}
+
+int main(){
+    int x = 45;
+    cout << sum(2,5);
+    return 0;
+}
+
+// OUTPUT: 7
+
+/* 
+Two Memory Types:
+1. Heap: For dynamic memory allocation.
+2. Stack: Static memory allocation.
+          Used in Functions.
+          |           |     
+          |           |
+          |___________|
+          |s=7   sum  | (This stack frame complete the work.
+          |___________|   after return value of s=7 frame automatically empty and used for new function call.)
+          |x=45  main |
+          |___________| (stack frame) When main return 0 this frame also empty.
+          
+          - Current function is at the top of the stack.
+          - When the main start execution the stack memory is allocated. At the top of stack the main is there.
+          - When main Call the Function that function come to the top of stack and gets new frame.
+          - In this manar the memory is allocated and empty after work done.
+
+*/
