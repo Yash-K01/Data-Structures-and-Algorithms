a0652d9307d32d4d750fce79d5d5538c484a41ff
Yash Khartode <yashkhartode1004@gmail.com>
2026-09-10 21:26:45 +0530

    Practice 2.

--- Ass4.cpp
+++ Ass4.cpp
@@ -39,12 +39,31 @@ bool isPrime(int n){
     return true;
 }
 
+// Function to find binomial coefficient for given n & r.
+int factorial(int n){
+    int fact = 1;              // n       n!      Suppose we have 4 subject from that choice is of 2.
+    for(int i=1; i<=n; i++){   //  C = -------    Posible combinations are: (1,2) (1,3) (1,4) (2,3) (2,4) (3,4) => 6
+        fact *= i;             //   r  r!(n-r)!   So calculating manually it  takes time so we use this formula.
+    }
+    return fact;               // 4        4!           24
+}                              //  C  = --------- => --------- = 6
+                               //   2    2!(4-2)!        4
+int binCoeff(int n, int r){
+    int val1 = factorial(n);
+    int val2 = factorial(r);
+    int val3 = factorial(n-r);
+
+    int result = val1 / (val2 * val3);
+    return result;
+}
+
 int main(){
     cout << prod(10, 20) << endl;
     cout << isEven(20) << endl;
     cout << isEven(19) << endl;
     fact(5);
     cout << isPrime(22) << endl;
+    cout << binCoeff(4, 2) << endl;
     return 0;
 }
 
@@ -56,4 +75,5 @@ PS C:\Users\Yash Khartode\Desktop\DSA C++> g++ Ass4.cpp ; ./a.exe
 0
 Factorial(5) = 120
 0
+6
 */
