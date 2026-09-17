bd321a016f433d737c2dd33b539db05da6b481b5
Yash Khartode <yashkhartode1004@gmail.com>
2026-09-17 21:52:53 +0530

    Dereference operator.

--- 16_Pointers.cpp
+++ 16_Pointers.cpp
@@ -3,14 +3,30 @@ using namespace std;
 
 int main(){
     int a = 10;
-    cout << &a << endl;
+    int *ptr = &a;
+    int **pptr = &ptr;
+    *ptr = 20;
+
+    float pi = 3.14;
+    float* ptr2 = &pi;
+
+    cout << &a << " = " << ptr << endl;
+    cout << &pi << " = " << ptr2 << endl;
+    cout <<"Pointer of Pointer: ";
+    cout << &ptr << " = " << pptr << endl;
+    cout << "Dereference of '*(&a)': " << *(&a) << endl;
+    cout << "New value of 'a': " << a << "\n";
     return 0;
 }
 
 /*
 OUTPUT:
 PS C:\Users\Yash Khartode\Desktop\DSA C++> g++ 16_Pointers.cpp ; ./a.exe         
-0x61ff0c
+0x61ff04 = 0x61ff04
+0x61fefc = 0x61fefc
+Pointer of Pointer: 0x61ff00 = 0x61ff00
+Dereference of '*(&a)': 10
+New value of 'a': 20
 */
 
 /*
@@ -26,4 +42,17 @@ Address of Operator: "&" gives the address of a variable.
 suppose a
        10       &a = 7920 -> in code brings address of variable 'a'.
       0x7920
+
+Pointers: A special variable that stores address of another variable.
+int *ptr = &a;
+variable ->  a          ptr                                        pptr
+value ->     10         0x100                                      0x500
+address ->   0x100      0x500                                      0x1000
+            int (4-B)   int (8-Bytes)<- Because it is pointer.     It is storing address of Pointer, So it is called Pointer of Pointer.
+
+Dereference Operator: Gets the value of the variable pointed by any pointer.
+* operator lets us directly access & modify the value of variable.
+int a = 10;
+int *ptr = &a;                *(address) <- Here when star come on any address it become a value store at that address.
+'*ptr' will be equal to 10.   [_value__]
 */
